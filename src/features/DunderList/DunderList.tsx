28360710fbe1f213517c6e75be43a9a1586f729d
Yondondon <roman17091993@gmail.com>
2023-07-30 18:48:44 +0300

    added search, filter

--- src/features/DunderList/DunderList.tsx
+++ src/features/DunderList/DunderList.tsx
@@ -1,9 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import Select from 'react-select';
 import { useGetDunderlistQuery } from '../api/apiSlice';
 import { DunderListItem } from './components/DunderListItem';
 import { ModalWindow } from '../../components/ModalWindow/ModalWindow';
 import { Loader } from '../../components/Loader/Loader';
-import { DatePicker } from '../../components/DatePicker/DatePicker';
+
+type SortOptionType = {
+  value: string;
+  label: string;
+}
+
+const sortOptions: SortOptionType[] = [
+  { value: 'date', label: 'Новизна' },
+  { value: 'heart', label: 'Лайки' },
+  { value: 'poop', label: 'Дизлайки' },
+]
 
 export const DunderList = () => {
   const {
@@ -13,15 +24,21 @@ export const DunderList = () => {
   } = useGetDunderlistQuery();
   const [showModal, setShowModal] = useState<boolean>(false);
   const [modalMsg, setModalMsg] = useState<string>('');
+  const [list, setList] = useState([]) as any[];
+  const [searchVal, setSearchVal] = useState<string>('');
+  const [sortFilter, setSortFilter] = useState(sortOptions[0]);
 
   let sortedDunderlist: any[] = [];
 
-  if(dunderlist && dunderlist.data.list.length > 0) {
-    sortedDunderlist = [...dunderlist.data.list]
-    sortedDunderlist.sort((a: any, b: any) => {
-      return b.created - a.created;
-    })
-  }
+  useEffect(() => {
+    if(dunderlist && dunderlist.data.list.length > 0) {
+      sortedDunderlist = [...dunderlist.data.list]
+      sortedDunderlist.sort((a: any, b: any) => {
+        return b.created - a.created;
+      })
+      setList(sortedDunderlist)
+    }
+  }, [dunderlist])
 
   useEffect(() => {
     if(isError) {
@@ -37,24 +54,71 @@ export const DunderList = () => {
     }
   })
 
+  useEffect(() => {
+    const arr = [...list];
+    switch(sortFilter.value) {
+      case 'date':
+        arr.sort((a: any, b: any) => {
+          return b.created - a.created;
+        });
+        break;
+      case 'heart':
+        arr.sort((a: any, b: any) => {
+          return b.reactions.heart - a.reactions.heart;
+        });
+        break;
+      case 'poop':
+        arr.sort((a: any, b: any) => {
+          return b.reactions.poop - a.reactions.poop;
+        });
+        break;
+      default:
+        return;
+    }
+    setList(arr)
+  }, [sortFilter])
+
+  const renderList = () => {
+    const filteredList = list.filter((item: any) => item.name.toLowerCase().includes(searchVal));
+    return filteredList.map((item: any) => {
+      return (
+        <DunderListItem
+          key={Math.random().toString()}
+          name={item.name}
+          imageUrl={item.imageUrl}
+          gameUrl={item.gameUrl}
+          reactions={item.reactions}
+          id={item.id}
+          appID={item.appID}
+          setModalText={(text: string) => setModalMsg(text)}
+          setShowModal={() => {setShowModal(true)}}
+        />
+      )
+    })
+  }
+
+  const handleSearch = (e: any) => {
+    setSearchVal(e.target.value.toLowerCase())
+  }
+
+  const handleSelect = (selectedOption: any) => {
+    setSortFilter(selectedOption)
+  }
+
   return (
     <div className='dunderlist_wrap'>
+      <input
+        type='text'
+        onChange={(e) => handleSearch(e)} value={searchVal}
+        placeholder='Пошук...'
+      />
+      <Select 
+        defaultValue={sortFilter}
+        onChange={handleSelect}
+        options={sortOptions}
+      />
       { isFetching && <Loader />}
-      { sortedDunderlist && sortedDunderlist.map((item: any) => {
-        return (
-          <DunderListItem
-            key={Math.random().toString()}
-            name={item.name}
-            imageUrl={item.imageUrl}
-            gameUrl={item.gameUrl}
-            reactions={item.reactions}
-            id={item.id}
-            appID={item.appID}
-            setModalText={(text: string) => setModalMsg(text)}
-            setShowModal={() => {setShowModal(true)}}
-          />
-        )
-      })}
+      { renderList() }
       { showModal && (
           <ModalWindow
             onClose={() => setShowModal(false)}
